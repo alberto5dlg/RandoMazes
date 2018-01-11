ea7d3023dd1f37dc0fe53dcc699dd5fb8fde01b4
alberto5dlg <alberto5dlg@gmail.com>
2018-01-11 09:54:44 +0100

    Initial Commit

---
+++ Assets/Lang.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Xml;
+
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public class Lang
+	{
+
+		private Hashtable Strings;
+
+		public Lang (String lang, String path)
+		{
+			setLanguage (lang, path);
+		}
+
+		public void setLanguage ( string path, string language) {
+			var xml = new XmlDocument();
+			xml.Load(path);
+
+			Strings = new Hashtable();
+			var element = xml.DocumentElement[language];
+			if (element != null) {
+				var elemEnum = element.GetEnumerator();
+				while (elemEnum.MoveNext()) {
+					var xmlItem = (XmlElement)elemEnum.Current;
+					Strings.Add(xmlItem.GetAttribute("name"), xmlItem.InnerText);
+				}
+			} else {
+				Debug.LogError("The specified language does not exist: " + language);
+			}
+		}
+
+		public string getString (string name) {
+			if (!Strings.ContainsKey(name)) {
+				Debug.LogError("The specified string does not exist: " + name);
+
+				return "";
+			}
+
+			return (string)Strings[name];
+		}
+	}
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		BeginGame ();
+		Debug.Log (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			
+			RestartGame ();
+		}
+	
+	}
+
+	private void BeginGame() {}
+	private void RestartGame() {}
+}
