14aba68776bac2e1a90627a4e5c384d4cbe175f2
alberto5dlg <alberto5dlg@gmail.com>
2018-01-16 10:52:38 +0100

    Generacion de celdas aleatorias del suelo del laberinto

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,17 @@ using RandoMazes.Languages;
 public class GameManager : MonoBehaviour {
 
 
+	public Maze mazePrefab; 
+
+	private Maze mazeInstance; 
+
 	private Lang language;  
 
 	// Use this for initialization
 	void Start () {
+		language = new Lang ("Spanish", "Assets/Languages/Spanish.xml");
 		BeginGame ();
-		language = new Lang ("Spanish", "Assets/Languages/Spanish.xml"); 
-		Debug.Log (language.getString ("welcome")); 
+		//Debug.Log (language.getString ("welcome"));  
 	}
 	
 	// Update is called once per frame
@@ -23,6 +27,13 @@ public class GameManager : MonoBehaviour {
 	
 	}
 
-	private void BeginGame() {}
-	private void RestartGame() {}
+	private void BeginGame() {
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		StartCoroutine(mazeInstance.Generate ()); 
+	}
+	private void RestartGame() {
+		StopAllCoroutines (); 
+		Destroy(mazeInstance.gameObject);
+		BeginGame();
+	}
 }
