337d0b2856409d263c0a6f6d507d18c0faf99bbb
alberto5dlg <alberto5dlg@gmail.com>
2018-01-16 11:47:41 +0100

    Implementado backtracking para la generacion de casillas del tablero

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Maze : MonoBehaviour {
 
@@ -16,15 +17,15 @@ public class Maze : MonoBehaviour {
 	public IEnumerator Generate() {
 		WaitForSeconds delay = new WaitForSeconds (generationStepDisplay);
 		cells = new MazeCell[size.x, size.z]; 
-		IntVector2 coordinates = RandomCoordinates; 
-		while (ContainsCoordinates (coordinates) && GetCell(coordinates) == null) {
-			yield return delay; 
-			CreateCell (coordinates); 
-			coordinates += MazeDirections.RandomValue.ToIntVector2();
+		List<MazeCell> activeCells = new List<MazeCell> ();
+		DoFirstGenerationStep (activeCells); 
+		while (activeCells.Count > 0) {
+			yield return delay;
+			DoNextGenerationStep (activeCells);
 		}
 	}
 
-	private void CreateCell (IntVector2 coordinates) {
+	private MazeCell CreateCell (IntVector2 coordinates) {
 		MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
 		cells[coordinates.x, coordinates.z] = newCell;
 		newCell.coordinates = coordinates; 
@@ -32,6 +33,23 @@ public class Maze : MonoBehaviour {
 		newCell.transform.parent = transform;
 		newCell.transform.localPosition = 
 			new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+		return newCell;
+	}
+
+	private void DoFirstGenerationStep (List<MazeCell> activeCells) {
+		activeCells.Add (CreateCell (RandomCoordinates)); 
+	}
+
+	private void DoNextGenerationStep (List<MazeCell> activeCells) {
+		int currentIndex = activeCells.Count - 1 ; 
+		MazeCell currentCell = activeCells [currentIndex];
+		MazeDirection direction = MazeDirections.RandomValue; 
+		IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2 ();
+		if (ContainsCoordinates (coordinates) && GetCell (coordinates) == null) {
+			activeCells.Add (CreateCell (coordinates));
+		} else {
+			activeCells.RemoveAt(currentIndex);
+		}
 	}
 
 	public IntVector2 RandomCoordinates {
